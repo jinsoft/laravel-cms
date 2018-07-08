1a268bc34a21503014a4ef29501570537b389ad6
xiao <jx_xiaojin@aliyun.com>
2018-07-08 23:08:14 +0800

    2018年7月8日

---
+++ app/Helps/Helpers.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaojin
+ * Email: job@ainiok.com
+ * Date: 2018/7/8 22:38
+ */
+if (!function_exists('human_filesize')) {
+    /**
+     * 返回可读性更好的文件尺寸
+     */
+    function human_filesize($bytes, $decimals = 2)
+    {
+        $size = ['B', 'kB', 'MB', 'GB', 'TB', 'PB'];
+        $factor = floor((strlen($bytes) - 1) / 3);
+
+        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$size[$factor];
+    }
+}
