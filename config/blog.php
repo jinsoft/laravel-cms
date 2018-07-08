1de76980482cfd4e8e1ba9bc020f11ef705c6b8e
xiao <jx_xiaojin@aliyun.com>
2018-07-08 23:56:39 +0800

    2018年7月8日

---
+++ config/blog.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'meta' => [
+        'keywords' => '1111',
+        'description' => 'blog'
+    ],
+    'default_ico' => 'fav.ico',
+    'footer' => [
+        'github' => [
+            'url' => 'www'
+        ],
+        'meta' => '1111'
+    ]
+];
---
+++ resources/views/article/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h3>2222</h3>
+@endsection
