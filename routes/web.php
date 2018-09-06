da5abb45cbe8adae693c740324b105c93eb73d93
xiao <jx_xiaojin@aliyun.com>
2018-09-07 00:22:44 +0800

    add blade template

--- routes/web.php
+++ routes/web.php
@@ -22,16 +22,18 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
     Route::get('login', 'AuthController@showLoginForm');
     Route::post('login', 'AuthController@login')->name('admin.login');
-    Route::post('logout','AuthController@logout')->name('admin.logout');
+    Route::post('logout', 'AuthController@logout')->name('admin.logout');
 
-    Route::group(['middleware'=>'auth:admin'],function(){
-        Route::get('/','AdminController@index')->name('admin');
-        Route::get('/dashboard','AdminController@dashboard')->name('admin.dashboard');
-        Route::get('/console','AdminController@console')->name('admin.console');
+    Route::group(['middleware' => 'auth:admin'], function () {
+        Route::get('/', 'AdminController@index')->name('admin');
+        Route::get('/dashboard', 'AdminController@dashboard')->name('admin.dashboard');
+        Route::get('/console', 'AdminController@console')->name('admin.console');
 
-        Route::get('/setting/smtp','SettingController@smtp')->name('admin.setting.smtp');
-        Route::get('/setting/info','SettingController@info')->name('admin.setting.info');
-        Route::post('/setting/{uuid}','SettingController@infoUpdate')->name('admin.setting.infoUpdate');
+        Route::get('/setting/smtp', 'SettingController@smtp')->name('admin.setting.smtp');
+        Route::get('/setting/info', 'SettingController@info')->name('admin.setting.info');
+        Route::post('/setting/{uuid}', 'SettingController@infoUpdate')->name('admin.setting.infoUpdate');
+        //消息
+        Route::get('/message', 'MessageController@index')->name('admin.message');
     });
 });
 
